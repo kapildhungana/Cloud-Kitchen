7f7e1d8f1b671e5a225652825ce035bfc5d33bc2
umsisamess <umeshsaud2001@gmail.com>
2022-04-18 21:06:17 +0530

    Adding kitchen personnel and delivery employee

--- Frontend/src/pages/Components/Board1.js
+++ Frontend/src/pages/Components/Board1.js
@@ -8,9 +8,12 @@ const Board1 = (props) => {
 
     const orderDone = async (item) => {
 
+        const user = localStorage.getItem("username");
+        item.kitchen = user;
+
         if(item.status === 0){
             const order_url = "http://localhost:5000/order/update0";
-            await axios.patch(order_url, item);
+            await axios.patch(order_url, item, user);
         }
         else if(item.status === 1){
             const order_url = "http://localhost:5000/order/update1";
@@ -33,8 +36,6 @@ const Board1 = (props) => {
                         </div>
                     )
                 }) }
-                {/* <div className="itemName">{item.orderDetails[0].name}</div>
-                <div className="itemName">Quantity Take</div> */}
             
             <div className="take" onClick = {() => {
                 orderDone(item);
